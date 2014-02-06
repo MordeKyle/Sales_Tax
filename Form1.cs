4733d2536897b73b7c9a7ef64a969825d1d40113
Kyle McBride <Kyle@MordeKyle.com>
2014-02-06 11:26:02 -0600

    Initial Commit
    Created Repository and description of program.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Program: Sales Tax Calculator
+//Author: Kyle McBride A02609917
+//Date: 02/06/2014
+//Description: The user of this program will enter the price of an item or items into the input. The
+//             program will then calculate county and state sales taxes. The sales tax will then be
+//             displayed to the user as county sales tax, state sales tax, the total amount of sales
+//             tax, and the total amount of the purchase.
+
+//                                                   *Change Log*
+//     01/06/2014     Repositor Creation.
+namespace Sales_Tax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
